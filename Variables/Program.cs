32574150b5c7396daf7fcd7e0df7a4b66768a0f2
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-03 16:28:03 -0600

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            Console.WriteLine(number);
+        }
+    }
+}
